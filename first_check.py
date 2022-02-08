3d13b0bde63ab39baa0b08e1c510d5905567cd24
Yeison <enmanuelquiterios@gmail.com>
2022-02-07 23:28:03 -0500

    Strings

--- first_check.py
+++ first_check.py
@@ -2,5 +2,12 @@
 my_age = 25
 my_dogs = ['Sammy', 'Linda',  52]
 
+no_change = (23, 34, 56)
+number_float = 2.5
+
+
 print(type(my_age))
 print(type(my_dogs))
+print(type(no_change))
+print(type(number_float))
+print(number_float * my_age)
