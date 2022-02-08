ae1e8ba6520abcd9dad1bb8303d9c59e3d4b1fc8
Yeison <enmanuelquiterios@gmail.com>
2022-02-07 23:25:08 -0500

    Adding Numbers type

--- first_check.py
+++ first_check.py
@@ -1,2 +1,6 @@
-print('Hello word welcome here')
-print('Hello friends here')
+'Naming variables in Python'
+my_age = 25
+my_dogs = ['Sammy', 'Linda',  52]
+
+print(type(my_age))
+print(type(my_dogs))
