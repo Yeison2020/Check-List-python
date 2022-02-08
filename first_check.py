1244359a5a7a15af3e6121101b31422853066672
Yeison <enmanuelquiterios@gmail.com>
2022-02-07 23:35:42 -0500

    Adding len and string slicing

--- first_check.py
+++ first_check.py
@@ -4,10 +4,14 @@ my_dogs = ['Sammy', 'Linda',  52]
 
 no_change = (23, 34, 56)
 number_float = 2.5
-
+name = 'Yeison'
 
 print(type(my_age))
 print(type(my_dogs))
 print(type(no_change))
 print(type(number_float))
 print(number_float * my_age)
+print(my_dogs[0:1])
+print('Hello world One')
+print('Hello \n world Two')
+print(len(name))
